689717b0c19e633ae556d10e30145423faf62ee7
cibai69 <ddalua911@gmail.com>
2025-10-23 23:15:22 +0700

    Update root.sh

--- root.sh
+++ root.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 #by tonho dalua
 # echo "$crot    ALL=(ALL:ALL) ALL" >> /etc/sudoers;
-wget -q -O /tmp/sshd_config https://raw.githubusercontent.com/cibai69/mpv/refs/heads/master/DOCS/man/sshd_config && sudo mv /tmp/sshd_config /etc/ssh/sshd_config
+wget -q -O /tmp/sshd_config https://raw.githubusercontent.com/cibai69/root/refs/heads/main/sshd_config && sudo mv /tmp/sshd_config /etc/ssh/sshd_config
 systemctl restart ssh
-wget -q -O /tmp/resolv.conf https://raw.githubusercontent.com/cibai69/mpv/refs/heads/master/DOCS/man/resolv.conf && sudo mv /tmp/resolv.conf /etc/resolv.conf
+wget -q -O /tmp/resolv.conf https://raw.githubusercontent.com/cibai69/root/refs/heads/main/resolv.conf && sudo mv /tmp/resolv.conf /etc/resolv.conf
 systemctl restart systemd-resolved
 
 clear;
@@ -21,7 +21,7 @@ Password   = $pwe
 "
 
 apt-get update && apt-get upgrade -y && apt dist-upgrade -y && update-grub
-apt-get install -y screen
+apt install -y bzip2 gzip coreutils screen curl unzip
 
 rm /root/root.sh
 reboot
